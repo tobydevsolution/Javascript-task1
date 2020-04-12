f856014f7a3981e4bdafa7597e896b26a961529c
Tobiloba Orekoya <tobilobaorekoya19@gmail.com>
2020-04-12 20:53:14 +0100

    My JS task1 commit

---
+++ mytask1.js
@@ -0,0 +1,25 @@
+const myName = "Tobiloba Orekoya";
+
+
+const myCourses = ["HTML","CSS","JAVASCRIPT","PHP"];
+const numOfCourses = myCourses.length;
+
+console.log(`My name is ${myName}`);
+console.log(`My courses are ${myCourses} `);
+let numDisplay = "";
+
+for(i=0;i<=200; i++) {
+    if(myCourses.length% 2 === i % 2) {
+        console.log(i);
+    }
+}
+    
+console.log(numDisplay);
+
+
+
+
+
+
+
+
